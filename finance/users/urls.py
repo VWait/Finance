576e660f0b29f5fd5f85cd19b1b1b737f20304e8
vlnovikov <v.novikov@clevermis.ru>
2024-09-11 10:45:06 +0500

    Прикрутил авторизацию

--- finance/users/urls.py
+++ finance/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import Login, Register
 
 urlpatterns = [
-    path("login/", views.Login.as_view())
+    path('login/', Login.as_view()),
+    path('register/', Register.as_view())
 ]
--- finance/users/views.py
+++ finance/users/views.py
@@ -1,12 +1,32 @@
 from django.views.generic import FormView
 
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 
 
 
-class Login(FormView):
+class Login(LoginView):
+    template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return self.request.GET.get('next', '/logs')
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data() | {'btn_success_value': 'Войти'}
+
+
+class Register(FormView):
     template_name = 'users/login.html'
     form_class = UserCreationForm
 
     def get_success_url(self):
         return self.request.GET.get('next', '/logs')
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data() | {'btn_success_value': 'Зарегистрироваться'}
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
